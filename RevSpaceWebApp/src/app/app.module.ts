60ecf17bb24bda6ce08a2e092a27a189ed3a0d81
Michael Than <than.mike@gmail.com>
2021-11-18 09:31:48 -0800

    duplicate import

--- RevSpaceWebApp/src/app/app.module.ts
+++ RevSpaceWebApp/src/app/app.module.ts
@@ -5,7 +5,6 @@ import { HttpClientModule } from '@angular/common/http'
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
 
-import { ViewProfileComponentComponent } from './components/view-profile-component/view-profile-component.component';
 import { PostFeedComponent } from './components/post-feed/post-feed.component';
 import { CreatePostComponent } from './components/create-post/create-post.component';
 import { PopulateFeedComponent } from './components/populate-feed/populate-feed.component';
