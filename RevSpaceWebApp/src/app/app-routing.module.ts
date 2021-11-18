fbbd47801590666b3228f919f938039c1b4c3913
prita5085 <62676367+prita5085@users.noreply.github.com>
2021-11-18 16:50:18 -0500

    added basic createPost/modified-tsconfig

--- RevSpaceWebApp/src/app/app-routing.module.ts
+++ RevSpaceWebApp/src/app/app-routing.module.ts
@@ -1,5 +1,6 @@
 import { NgModule } from '@angular/core';
 import { RouterModule, Routes } from '@angular/router';
+import { CreatePostComponent } from './components/create-post/create-post.component';
 import { PostFeedComponent } from './components/post-feed/post-feed.component';
 
 
