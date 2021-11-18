24f045a370b8c7a41bf2b4ad6b24ff5e1183e762
RyanWhitehill-Rev <ryan.whitehill@revature.net>
2021-11-18 15:41:39 -0700

    Filled html with input tags, placeholder methods for assigning and collecting input values. Not in a state to merge with develop yet.

--- RevSpaceWebApp/src/app/components/edit-user-profile/edit-user-profile.component.ts
+++ RevSpaceWebApp/src/app/components/edit-user-profile/edit-user-profile.component.ts
@@ -1,4 +1,5 @@
 import { Component, OnInit } from '@angular/core';
+import { User } from 'src/app/models/User';
 
 @Component({
   selector: 'app-edit-user-profile',
@@ -7,9 +8,92 @@ import { Component, OnInit } from '@angular/core';
 })
 export class EditUserProfileComponent implements OnInit {
 
-  constructor() { }
+  constructor() { 
 
-  ngOnInit(): void {
+    let dateObject = new Date();
+    let currentYear = dateObject.getFullYear();
+    let currentMonth = (dateObject.getMonth() + 1).toString().padStart(2, '0');
+    let currentDay = dateObject.getDate().toString().padStart(2, '0');
+
+    this.currentDateString = currentYear + "-" + currentMonth + "-" + currentDay;
+    this.minimumBirthdayString = (currentYear-125) + "-" + currentMonth + "-" + currentDay;
+
+    this.resetInputFields();
+
   }
 
+  ngOnInit(): void {}
+
+  //Variable declarations 
+  currentDateString:String;
+  minimumBirthdayString: String;
+
+  firstNameInput: String = "";
+  lastNameInput: String = "";
+  titleInput: String = "";
+  githubUsernameInput: String = "";
+  joinDateInput: Date = new Date('Jan 1, 2010');
+  locationInput: String = "";
+  aboutMeInput:String = "";
+  birthdayInput: Date  = new Date('Jan 1, 2000');
+
+
+  //Sets the input variables equal to the logged in user's current profile data
+  //Call this when opening/changing to the edit profile screen
+  //Currently uses (hardcoded) placeholder data - refactor once actual user data is available
+  resetInputFields() {
+
+    this.firstNameInput = "PlaceholderFirstname";
+    this.lastNameInput = "PlaceholderLastName";
+    this.titleInput = "Placeholder Title";
+    this.githubUsernameInput = "Placeholder GitHubuserName";
+    this.joinDateInput = new Date('Jan 1, 2015');
+    this.locationInput = "Placeholder Location";
+    this.aboutMeInput = "Placeholder AboutMe";
+    this.birthdayInput = new Date('Jan 1, 2000');
+
+  }
+
+
+  //Bundles the input values into a User object, then sends the User object to the service
+  //The service will create the request itself
+  //Finalize implementation based on what the backend wants for the update request
+  confirmUpdate() {
+
+    console.log("Join date representation:");
+    console.log(this.joinDateInput);
+    console.log((new Date(this.joinDateInput)).getTime());
+
+    console.log("Birthday representation:");
+    console.log(this.birthdayInput)
+    console.log((new Date(this.birthdayInput)).getTime());
+
+    let fakeUser = {
+      firstName: this.firstNameInput,
+      lastName: this.lastNameInput,
+      birthday: (new Date(this.birthdayInput)).getTime(),
+      revatureJoinDate: (new Date(this.joinDateInput)).getTime(),
+      githubUsername: this.githubUsernameInput,
+      title: this.titleInput,
+      location: this.locationInput,
+      aboutMe: this.aboutMeInput,
+    };
+
+    console.log(fakeUser);
+    
+
+  }
+
+
+
+  //Reset the fields and return to the view user profile screen
+  cancelChanges() {
+
+  }
+
+  
+
+
+
 }
+
