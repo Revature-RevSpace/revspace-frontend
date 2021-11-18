a38636331b23006b5264e32c3e45eac78da78b72
Joseph B <josephbettendorff@yahoo.com>
2021-11-18 13:54:39 -0600

    Revert "Merge pull request #14 from Revature-RevSpace/viewProfileFeature"
    This reverts commit de7dfc5655477e2a1aeeefcd216347a07e3effa0, reversing
    changes made to 32aca3fabe4b1128385c0bb0ccddb65dc849d377.

--- RevSpaceWebApp/src/app/app.module.ts
+++ RevSpaceWebApp/src/app/app.module.ts
@@ -4,21 +4,16 @@ import { HttpClientModule } from '@angular/common/http'
 
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
-
 import { PostFeedComponent } from './components/post-feed/post-feed.component';
 import { CreatePostComponent } from './components/create-post/create-post.component';
 import { PopulateFeedComponent } from './components/populate-feed/populate-feed.component';
-import { ViewProfileComponentComponent } from './components/view-profile-component/view-profile-component.component';
-
 
 @NgModule({
   declarations: [
     AppComponent,
-    ViewProfileComponentComponent,
     PostFeedComponent,
     CreatePostComponent,
-    PopulateFeedComponent,
-    ViewProfileComponentComponent
+    PopulateFeedComponent
   ],
   imports: [
     BrowserModule,
