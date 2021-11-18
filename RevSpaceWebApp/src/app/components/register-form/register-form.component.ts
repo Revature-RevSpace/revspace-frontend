51e94c2ec4c0768f189665e0fa6dc563c776e14b
dreichle23 <devin.reichle@revature.net>
2021-11-18 13:53:07 -0800

    fixed approuting

--- RevSpaceWebApp/src/app/components/register-form/register-form.component.ts
+++ RevSpaceWebApp/src/app/components/register-form/register-form.component.ts
@@ -10,6 +10,59 @@ export class RegisterFormComponent implements OnInit {
   constructor() { }
 
   ngOnInit(): void {
+   
   }
+  userFirstName: string;
+  userLastName: string;
+  userEmail: string;
+  userDob: string;
+  userPassword: string;
+  userConfirmPassword: string;
+  userDOBNum: number;
+  differentPasswords: string = "Passwords do not match";
+  noFirstName: string = "Please Input Valid First Name";
+  noLastName: string = "Please Input Valid Last Name";
+  noEmail: string  = "Please Input Valid Email";
+  noDob: string = "Please Valid Input Date of Birth";
+  correctInfo: boolean;
+
+
+
+  
+  
+  Register(){
+    if (this.userPassword != this.userConfirmPassword){
+      
+      this.correctInfo = false;
+    } else if (!this.userEmail){
+      
+      this.correctInfo = false;
+
+    } else if (!this.userFirstName){
+      
+      this.correctInfo = false;
+    } else if (!this.userLastName){
+      
+      this.correctInfo = false;
+    }else if (!this.userDob){
+      
+      this.correctInfo = false;
+    }
+    else{
+      this.correctInfo = true;
+      
+  }
+  if(this.correctInfo){
+    console.log(this.userFirstName);
+      console.log(this.userLastName);
+      console.log(this.userEmail);
+      console.log(this.userDob);
+      console.log(this.userPassword);
+      console.log(this.userConfirmPassword);
+  }
+    
+  }
+
+
 
 }
