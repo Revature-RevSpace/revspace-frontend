0796c1701797618891cd0fe7fcdc9a4be9071eba
Michael Than <than.mike@gmail.com>
2021-11-18 08:57:27 -0800

    view profile component

--- RevSpaceWebApp/src/app/app.module.ts
+++ RevSpaceWebApp/src/app/app.module.ts
@@ -9,6 +9,7 @@ import { ViewProfileComponentComponent } from './components/view-profile-compone
 import { PostFeedComponent } from './components/post-feed/post-feed.component';
 import { CreatePostComponent } from './components/create-post/create-post.component';
 import { PopulateFeedComponent } from './components/populate-feed/populate-feed.component';
+import { ViewProfileComponentComponent } from './components/view-profile-component/view-profile-component.component';
 
 
 @NgModule({
@@ -17,7 +18,8 @@ import { PopulateFeedComponent } from './components/populate-feed/populate-feed.
     ViewProfileComponentComponent,
     PostFeedComponent,
     CreatePostComponent,
-    PopulateFeedComponent
+    PopulateFeedComponent,
+    ViewProfileComponentComponent
   ],
   imports: [
     BrowserModule,
