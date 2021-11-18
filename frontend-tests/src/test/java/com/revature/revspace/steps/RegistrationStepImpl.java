d477e858e1564661793daf44fbbbf60d76991b18
dreichle23 <devin.reichle@revature.net>
2021-11-18 07:41:52 -0800

    added step impl for registration

---
+++ frontend-tests/src/test/java/com/revature/revspace/steps/RegistrationStepImpl.java
@@ -0,0 +1,91 @@
+package com.revature.revspace.steps;
+
+import io.cucumber.java.en.Given;
+import io.cucumber.java.en.Then;
+import io.cucumber.java.en.When;
+
+public class RegistrationStepImpl {
+    @Given("User is on the registration page")
+    public void user_is_on_the_registration_page() {
+        // Write code here that turns the phrase above into concrete actions
+        throw new io.cucumber.java.PendingException();
+    }
+
+    @When("User enters valid name")
+    public void user_enters_valid_name() {
+        // Write code here that turns the phrase above into concrete actions
+        throw new io.cucumber.java.PendingException();
+    }
+
+    @When("enters valid email")
+    public void enters_valid_email() {
+        // Write code here that turns the phrase above into concrete actions
+        throw new io.cucumber.java.PendingException();
+    }
+
+    @When("enters valid birthday")
+    public void enters_valid_birthday() {
+        // Write code here that turns the phrase above into concrete actions
+        throw new io.cucumber.java.PendingException();
+    }
+
+    @When("enters valid password")
+    public void enters_valid_password() {
+        // Write code here that turns the phrase above into concrete actions
+        throw new io.cucumber.java.PendingException();
+    }
+
+    @When("enters valid confirms password")
+    public void enters_valid_confirms_password() {
+        // Write code here that turns the phrase above into concrete actions
+        throw new io.cucumber.java.PendingException();
+    }
+
+    @When("clicks on register button")
+    public void clicks_on_register_button() {
+        // Write code here that turns the phrase above into concrete actions
+        throw new io.cucumber.java.PendingException();
+    }
+
+    @Then("User has successfully registered")
+    public void user_has_successfully_registered() {
+        // Write code here that turns the phrase above into concrete actions
+        throw new io.cucumber.java.PendingException();
+    }
+
+    @When("presses Enter")
+    public void presses_enter() {
+        // Write code here that turns the phrase above into concrete actions
+        throw new io.cucumber.java.PendingException();
+    }
+
+    @When("enters invalid confirms password")
+    public void enters_invalid_confirms_password() {
+        // Write code here that turns the phrase above into concrete actions
+        throw new io.cucumber.java.PendingException();
+    }
+
+    @Then("User has unsuccessfully registered")
+    public void user_has_unsuccessfully_registered() {
+        // Write code here that turns the phrase above into concrete actions
+        throw new io.cucumber.java.PendingException();
+    }
+
+    @When("enters invalid email")
+    public void enters_invalid_email() {
+        // Write code here that turns the phrase above into concrete actions
+        throw new io.cucumber.java.PendingException();
+    }
+
+    @When("User enters nothing")
+    public void user_enters_nothing() {
+        // Write code here that turns the phrase above into concrete actions
+        throw new io.cucumber.java.PendingException();
+    }
+
+    @When("clicks register button")
+    public void clicks_register_button() {
+        // Write code here that turns the phrase above into concrete actions
+        throw new io.cucumber.java.PendingException();
+    }
+}
