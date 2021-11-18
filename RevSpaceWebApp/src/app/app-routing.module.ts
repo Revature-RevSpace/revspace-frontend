1569b0584436896ed08c43aace985cbd37da7cc1
dreichle23 <devin.reichle@revature.net>
2021-11-18 13:53:07 -0800

    fixed approuting

--- RevSpaceWebApp/src/app/app-routing.module.ts
+++ RevSpaceWebApp/src/app/app-routing.module.ts
@@ -1,9 +1,10 @@
 import { NgModule } from '@angular/core';
 import { RouterModule, Routes } from '@angular/router';
 import { PostFeedComponent } from './components/post-feed/post-feed.component';
-
+import { RegisterFormComponent } from './components/register-form/register-form.component';
 
 const routes: Routes = [
+  {path: "register", component: RegisterFormComponent},
   { path: 'postfeed', component: PostFeedComponent }
 ];
 
