2c1237880f8c94df2a0901eb0152b6e37176c3b2
tlatham54 <tlatham54@gmail.com>
2021-11-18 09:59:48 -0500

    Adding some structure to the post feed component.

--- RevSpaceWebApp/src/app/app.module.ts
+++ RevSpaceWebApp/src/app/app.module.ts
@@ -3,10 +3,12 @@ import { BrowserModule } from '@angular/platform-browser';
 
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
+import { PostFeedComponent } from './components/post-feed/post-feed.component';
 
 @NgModule({
   declarations: [
-    AppComponent
+    AppComponent,
+    PostFeedComponent
   ],
   imports: [
     BrowserModule,
