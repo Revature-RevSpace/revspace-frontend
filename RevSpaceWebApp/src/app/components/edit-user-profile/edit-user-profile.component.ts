e28875ee337a45c3703cdf2aacaf98c4e73961e5
Meli Martinez <melisa.martinez.pro@gmail.com>
2021-11-19 01:00:32 -0600

    added new way to make Date -> num val

--- RevSpaceWebApp/src/app/components/edit-user-profile/edit-user-profile.component.ts
+++ RevSpaceWebApp/src/app/components/edit-user-profile/edit-user-profile.component.ts
@@ -33,10 +33,19 @@ export class EditUserProfileComponent implements OnInit {
   lastNameInput: String = "";
   titleInput: String = "";
   githubUsernameInput: String = "";
-  joinDateInput: Date = new Date('Jan 1, 2010');
+
+  // trying to get number date to work
+  // joinDateInput: Date = new Date('Jan 1, 2010');
+  joinDateInput: number = 0;
+
   locationInput: String = "";
   aboutMeInput:String = "";
   birthdayInput: Date = new Date('Jan 1, 2000');
+
+  // turns Date data type into epoch unix time
+  epoch (date: any) {
+    return Date.parse(date)
+  }
 
 
   //Sets the input variables equal to the logged in user's current profile data
@@ -48,7 +57,17 @@ export class EditUserProfileComponent implements OnInit {
     this.lastNameInput = "PlaceholderLastName";
     this.titleInput = "Placeholder Title";
     this.githubUsernameInput = "Placeholder GitHubuserName";
-    this.joinDateInput = new Date('Jan 1, 2015');
+
+    // converting epoch into date
+    // although I'm not sure we need to do this
+    // our date input is probably smart enough to take in the timestamp in its epoch unix value
+    // but just in case it's harder than I anticipate
+
+    let joinUnixDate = 946706400000;
+    let joinDate = new Date(joinUnixDate + 100000000);
+    let timestamp = this.epoch(joinDate);
+    this.joinDateInput = timestamp;
+
     this.locationInput = "Placeholder Location";
     this.aboutMeInput = "Placeholder AboutMe";
     this.birthdayInput = new Date('Jan 1, 2000');
@@ -63,7 +82,20 @@ export class EditUserProfileComponent implements OnInit {
 
     console.log("Join date representation:");
     console.log(this.joinDateInput);
-    console.log((new Date(this.joinDateInput)).getTime());
+
+    // converting the Date into a number type
+    // will be helpful for storing in our DB
+    let timestamp = this.epoch(this.joinDateInput);
+    console.log(timestamp);
+
+    // this doesn't quite make sense here but
+    // if we want to turn our timestamp into a Date to display
+    let newDate = new Date(timestamp + 100000000);
+    console.log(newDate);
+
+    // we can also use a Pipe in our HTML to avoid having to make another Date variable simply for displaying purposes
+    // in out HTML file, we would have:
+    // {{all.departureDateTime | date: 'MMMM d, y'}}
 
     console.log("Birthday representation:");
     console.log(this.birthdayInput);
@@ -73,7 +105,8 @@ export class EditUserProfileComponent implements OnInit {
       firstName: this.firstNameInput,
       lastName: this.lastNameInput,
       birthday: (new Date(this.birthdayInput)).getTime(),
-      revatureJoinDate: (new Date(this.joinDateInput)).getTime(),
+      revatureJoinDate: (timestamp),
+      // revatureJoinDate: (new Date(this.joinDateInput)).getTime(),
       githubUsername: this.githubUsernameInput,
       title: this.titleInput,
       location: this.locationInput,
