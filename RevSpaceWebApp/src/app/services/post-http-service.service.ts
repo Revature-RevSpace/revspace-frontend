4aff28fcea0eae44073a18817fa12d225e58346c
tlatham54 <tlatham54@gmail.com>
2021-11-20 18:20:48 -0500

    Further implementation of post feed.

--- RevSpaceWebApp/src/app/services/post-http-service.service.ts
+++ RevSpaceWebApp/src/app/services/post-http-service.service.ts
@@ -9,12 +9,21 @@ export class PostHttpServiceService {
 
   constructor(private http: HttpClient) { }
 
-  // authToken: string = "Basic " + btoa(user.email + password);
-  authToken: string = "a";
+  authToken: string = "Basic " + btoa("username1@email.com:Password1");
+
+  //authToken: string = "Basic " + btoa(user.email + user.password);
+
+  
   private authHeaders = new HttpHeaders({ 'Context-Type': 'application/json', 'Authorization': this.authToken});
 
   getTenPosts(oldestId: number): Observable<any>{
-    return this.http.get(`http://localhost:8080/posts/${oldestId}`, {headers: this.authHeaders});
+
+    let oldestIdString: string = "" + oldestId;
+
+    let authHeadersTen = new HttpHeaders({ 'Context-Type': 'application/json', 'Authorization': this.authToken, 'lastPostIdOnThePage': oldestIdString});
+
+    return this.http.get(`http://localhost:8080/posts`, {headers: authHeadersTen}
+    );
   }
 
   
