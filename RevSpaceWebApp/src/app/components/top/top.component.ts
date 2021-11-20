c6ed82c8eb6eae8e66f789021c19858e9f361583
HuangyingruiWang <whyrchris@hotmail.com>
2021-11-20 12:11:00 -0600

    fix some conflits

---
+++ RevSpaceWebApp/src/app/components/top/top.component.ts
@@ -0,0 +1,37 @@
+import { Component, OnInit, Inject, HostListener } from '@angular/core';
+import { DOCUMENT } from "@angular/common";
+
+@Component({
+  selector: 'app-top',
+  templateUrl: './top.component.html',
+  styleUrls: ['./top.component.css']
+})
+
+
+export class TopComponent implements OnInit {
+  windowScrolled: boolean = false;
+  constructor(@Inject(DOCUMENT) private document: Document) { }
+  
+  ngOnInit(): void {
+  }
+
+
+  @HostListener("window:scroll", [])
+    onWindowScroll() {
+        if (window.pageYOffset || document.documentElement.scrollTop || document.body.scrollTop > 100) {
+            this.windowScrolled = true;
+        } 
+       else if (this.windowScrolled && window.pageYOffset || document.documentElement.scrollTop || document.body.scrollTop < 10) {
+            this.windowScrolled = false;
+        }
+    }
+    scrollToTop() {
+        (function smoothscroll() {
+            var currentScroll = document.documentElement.scrollTop || document.body.scrollTop;
+            if (currentScroll > 0) {
+                window.requestAnimationFrame(smoothscroll);
+                window.scrollTo(0, currentScroll - (currentScroll / 8));
+            }
+        })();
+    }
+}
