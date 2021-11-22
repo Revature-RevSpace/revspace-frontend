eaec652f1ca2167da7ad7b55bd8bcbf8c5221cc6
mgvalenzu <51139227+mgvalenzu@users.noreply.github.com>
2021-11-22 08:53:07 -0800

    tether register component to register button on login page

--- RevSpaceWebApp/src/app/login/login.component.ts
+++ RevSpaceWebApp/src/app/login/login.component.ts
@@ -1,10 +1,13 @@
-import { Component, OnInit } from '@angular/core';
+import { Component, NgModule, OnInit } from '@angular/core';
 
 @Component({
   selector: 'app-login',
-  templateUrl: './login.component.html',
+  templateUrl: './login.component.html', 
   styleUrls: ['./login.component.css']
 })
+
+
+
 export class LoginComponent implements OnInit {
 
   constructor() { }
