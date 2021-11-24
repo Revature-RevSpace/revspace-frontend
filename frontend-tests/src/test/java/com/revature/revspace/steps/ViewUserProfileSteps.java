4311d7eaa14d191e136e1e10b9c72e9c55a9f8cd
Michael Than <than.mike@gmail.com>
2021-11-24 12:09:11 -0800

    added tests

---
+++ frontend-tests/src/test/java/com/revature/revspace/steps/ViewUserProfileSteps.java
@@ -0,0 +1,36 @@
+package com.revature.revspace.steps;
+
+import com.revature.revspace.CucumberRunner;
+import io.cucumber.java.en.Given;
+import io.cucumber.java.en.Then;
+import io.cucumber.java.en.When;
+import org.junit.jupiter.api.Assertions;
+import org.openqa.selenium.By;
+import org.openqa.selenium.WebElement;
+
+public class ViewUserProfileSteps {
+    @Given("user is logged in")
+    public void user_is_logged_in() {
+        CucumberRunner.DRIVER.manage().window().maximize();
+        WebElement usernameInput = CucumberRunner.DRIVER.findElement(By.id("email"));
+        usernameInput.clear();
+        usernameInput.sendKeys("username1@email.com");
+        WebElement passwordInput = CucumberRunner.DRIVER.findElement(By.id("password"));
+        passwordInput.clear();
+        passwordInput.sendKeys("Password1");
+        CucumberRunner.DRIVER.findElement(By.id("login")).click();
+
+    }
+    @When("user clicks on their profile")
+    public void user_clicks_on_their_profile() {
+        WebElement dropDown = CucumberRunner.DRIVER.findElement(By.xpath("/html/body/app-root/nav/div/div/div/button"));
+        dropDown.click();
+        WebElement userProfileButton = CucumberRunner.DRIVER.findElement(By.xpath("/html/body/app-root/nav/div/div/div/ul/li[1]/a"));
+        userProfileButton.click();
+    }
+    @Then("user profile is displayed")
+    public void user_profile_is_displayed() {
+        WebElement userFirstname = CucumberRunner.DRIVER.findElement(By.xpath("/html/body/app-root/app-view-profile-component/div/div[2]/div/div[1]/div/h3"));
+        Assertions.assertEquals("Revature Information", userFirstname.getText());
+    }
+}
--- frontend-tests/src/test/resources/com/revature/revspace/features/ViewUserProfile.feature
+++ frontend-tests/src/test/resources/com/revature/revspace/features/ViewUserProfile.feature
@@ -1,10 +1,5 @@
 Feature: view user profile
 
-  Scenario: user profile is displayed without being logged in
-    Given user is not logged in
-    When user clicks link to a user profile
-    Then user profile is shown
-
   Scenario: user views their own profile
     Given user is logged in
     When user clicks on their profile
