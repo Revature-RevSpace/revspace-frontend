285a45e14f45b37bdf21c668883f60fce16c09bf
prita5085 <62676367+prita5085@users.noreply.github.com>
2021-11-29 18:52:25 -0500

    createPostStepsTest

---
+++ frontend-tests/src/test/java/com/revature/revspace/steps/CreatePostSteps.java
@@ -0,0 +1,53 @@
+package com.revature.revspace.steps;
+
+import org.junit.jupiter.api.Assertions;
+import org.openqa.selenium.WebDriver;
+
+import com.revature.revspace.CucumberRunner;
+import com.revature.revspace.pages.LoginPage;
+import com.revature.revspace.pages.PostFeedPage;
+
+import io.cucumber.java.en.Then;
+import io.cucumber.java.en.When;
+
+public class CreatePostSteps {
+	
+	public static PostFeedPage postFeedPage = CucumberRunner.postFeedPage;
+    public static LoginPage loginPage = CucumberRunner.loginPage;
+    public static WebDriver driver = CucumberRunner.DRIVER;
+    public static String url = CucumberRunner.WEB_APP_URL;
+    public static String postMsg = "Hi, how are you?";
+    public static String imagePath = "C:\\Fetch\\tree.jpg";
+	
+	
+	@When("User enters a post")
+	public void user_enters_a_post() {
+		postFeedPage.newPostTextarea.click();
+		postFeedPage.newPostTextarea.sendKeys(postMsg);
+	    
+	}
+	@When("User uploads an image")
+	public void user_uploads_an_image() {
+		    
+	    postFeedPage.newPostImageInput.sendKeys(imagePath);
+	    	try {
+				Thread.sleep(5000);
+			} catch (InterruptedException e) {
+				// TODO Auto-generated catch block
+				e.printStackTrace();
+			}
+
+	}
+	
+	@When("User clicks on post button")
+	public void user_clicks_on_post_button() {
+	    postFeedPage.createPostBtn.click();
+	}
+	
+	@Then("Post will be successfully created")
+	public void post_will_be_successfully_created() {
+		Assertions.assertEquals(postFeedPage.firstPostMsg.getText(), postMsg);
+	   
+	}
+
+}
