7f90615b535d4ceba462c61708e82c2ef53b7a0b
erandy <burciaga.erandy@yahoo.com>
2022-03-03 13:49:29 -0700

    password component

--- RevSpaceWebApp/src/app/services/user.service.ts
+++ RevSpaceWebApp/src/app/services/user.service.ts
@@ -46,4 +46,8 @@ export class UserService {
   }
   */
 
+  changePassword(email:string): Observable<User>{
+    return this.http.post<User>(this.backendService.getBackendURL() + '/users/password', email, {headers: this.headers });
+  }
+
 }
