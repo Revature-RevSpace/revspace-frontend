cfa8c2414a752784c9c1cf2ea17bb0e780c3252c
Andoson22 <andersonnm@appstate.edu>
2022-03-07 11:01:17 -0500

    current changes

--- RevSpaceWebApp/src/app/components/view-profile-component/view-profile-component.component.ts
+++ RevSpaceWebApp/src/app/components/view-profile-component/view-profile-component.component.ts
@@ -1,7 +1,7 @@
 import { Component, OnInit } from '@angular/core';
 import { HttpClient, HttpHeaders } from '@angular/common/http';
 import { User } from 'src/app/models/User';
-import { ActivatedRoute } from '@angular/router';
+import { ActivatedRoute, Router } from '@angular/router';
 import { UserService } from 'src/app/services/user.service';
 import { LoginService } from 'src/app/services/login.service';
 
@@ -14,7 +14,7 @@ export class ViewProfileComponentComponent implements OnInit {
 
   // to get to this component the path would be viewprofile/:userid
 
-  constructor(private route: ActivatedRoute, private userHTTP: UserService, private loginUser: LoginService) { }
+  constructor(private router: Router, private route: ActivatedRoute, private userHTTP: UserService, private loginUser: LoginService) { }
 
   // user:User= new User(1,'email','Ryan','Schlientz',120395,2017,'https://github.com/Revature-RevSpace/revspace-application','Senior Trainer','West Virginia',"Lorem Ipsum is simply dummy text of the printing and typesetting industry. Lorem Ipsum has been the industry's standard dummy text ever since the 1500s, when an unknown printer took a galley of type and scrambled it to make a type specimen book. It has survived not only five centuries, but also the leap into electronic typesetting, remaining essentially unchanged. It was popularised in the 1960s with the release of Letraset sheets containing Lorem Ipsum passages, and more recently with desktop publishing software like Aldus PageMaker including versions of Lorem Ipsum.");
   user: User;
@@ -43,5 +43,35 @@ export class ViewProfileComponentComponent implements OnInit {
   // getUser(): User{
     
   // }
+
+  async init() {
+    console.log(1);
+    await this.sleep(1000);
+    console.log(2);
+  }
+  
+  sleep(ms) {
+    return new Promise((resolve) => {
+      setTimeout(resolve, ms);
+    });
+  }
+
+  follow() {
+    const authToken:string = this.loginUser.getLoginInfo().authToken;
+    const myHeaders:HttpHeaders = new HttpHeaders({
+    'Authorization': authToken
+      });
+    this.userHTTP.followUser(this.user.userId, this.loginUser.getLoginInfo().user, myHeaders).subscribe(
+      (response) => {
+        if(null != response) {
+          //As long as the response isn't null, the operation succeeded
+          //Set the user info in the front end to the new data (to avoid an extra backend call)
+          //Go the user profile page
+          this.router.navigate(["viewprofile/" + this.user.userId]);
+      }
+    });
+
+  }
+
 
 }
