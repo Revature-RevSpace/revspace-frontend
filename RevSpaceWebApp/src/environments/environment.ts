0515fc5975c48429ba6f21e331debb106d0c747a
ChriisM4 <96087662+ChriisM4@users.noreply.github.com>
2022-03-08 16:02:59 -0500

    pop up

--- RevSpaceWebApp/src/environments/environment.ts
+++ RevSpaceWebApp/src/environments/environment.ts
@@ -3,7 +3,7 @@
 // The list of file replacements can be found in `angular.json`.
 
 export const environment = {
-  backendURL: "http://localhost:8080",
+  backendURL: "http://localhost:9001",
   production: false
 };
 
