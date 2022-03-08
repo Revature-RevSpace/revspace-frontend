13b2f2ac34e0becabd962befe35b8e6e6dd3783b
steven_hansen <srhansen849@gmail.com>
2022-03-08 17:37:55 -0500

    search table

--- RevSpaceWebApp/src/app/components/search-bar/search-bar.component.ts
+++ RevSpaceWebApp/src/app/components/search-bar/search-bar.component.ts
@@ -3,6 +3,9 @@ import { Component, OnInit, Output } from '@angular/core';
 import { FormControl } from '@angular/forms';
 import { debounceTime, distinctUntilChanged, startWith } from 'rxjs/operators';
 import{Router,NavigationExtras} from '@angular/router';
+import { UserService } from 'src/app/services/user.service';
+import { SearchBarService } from 'src/app/services/search-bar.service';
+import { User } from 'src/app/models/User';
 @Component({
   selector: 'app-search-bar',
   templateUrl: './search-bar.component.html',
@@ -10,24 +13,26 @@ import{Router,NavigationExtras} from '@angular/router';
 })
 export class SearchBarComponent implements OnInit {
 
-  searchText:FormControl;
+  searchText:string;
 
-  search = "";
-  
-  User=[{}];
+  result: User[] = [];
 
-  constructor(private router:Router) {}
+  emstr = "@";
+  spstr = " ";
+
+  constructor(private router:Router, private sServ: SearchBarService) {}
   ngOnInit(): void {
     
   }
-  getAllUser(result:any){
-    const navigationExtras :NavigationExtras={
-      queryParams:{
-        result: JSON.stringify(result)
-      }
-    }
-    this.router.navigate(['search-details'], navigationExtras);
-  }
+
+  // getAllUser(result:any){
+  //   const navigationExtras :NavigationExtras={
+  //     queryParams:{
+  //       result: JSON.stringify(result)
+  //     }
+  //   }
+  //   this.router.navigate(['search-details'], navigationExtras);
+  // }
 
   // searchEmail(email: string){
   //   this.actRoute.queryParams.subscribe(data =>{
@@ -36,9 +41,29 @@ export class SearchBarComponent implements OnInit {
   //   });
   // }
 
-
-  onKeyUp(x) { // appending the updated value to the variable
-    
+  searchUsers(){
+    if(this.searchText.includes(this.emstr)){
+      this.sServ.searchUserByEmail(this.searchText).subscribe(
+        response => {
+          this.result = response;
+          this.sServ.searchedUsers = response;
+          this.router.navigate(['search-details']);
+        }
+      )
+    }else if(this.searchText.includes(this.spstr)){
+      let name = this.searchText.split(" ", 2);
+      let fname = name[0];
+      let lname = name[1];
+      this.sServ.searchUserByName(fname, lname).subscribe(
+        response => {
+          this.result = response;
+          this.sServ.searchedUsers = response;
+          this.router.navigate(['search-details']);
+        }
+      )
+    }else {
+      
+    }
   }
 
 }
--- RevSpaceWebApp/src/app/components/search-details/search-details.component.ts
+++ RevSpaceWebApp/src/app/components/search-details/search-details.component.ts
@@ -1,7 +1,9 @@
 import { Component, OnInit } from '@angular/core';
+import { Router } from '@angular/router';
 import { User } from 'src/app/models/User';
 import { LoginService } from 'src/app/services/login.service';
 import { SearchBarService } from 'src/app/services/search-bar.service';
+import { UserService } from 'src/app/services/user.service';
 
 @Component({
   selector: 'app-search-details',
@@ -15,23 +17,36 @@ export class SearchDetailsComponent implements OnInit {
   emstr = "@";
   spstr = " ";
 
-  constructor(private sServ: SearchBarService, private lServ: LoginService) { }
+  constructor(private sServ: SearchBarService, private lServ: LoginService, private router:Router, private uServ: UserService) { }
 
   ngOnInit(): void {
-    
+    this.userList = this.sServ.searchedUsers;
   }
 
-  findAPerson(searchResult: string){
-    if(searchResult.includes(this.emstr)){
-      this.sServ.searchUserByEmail(searchResult).subscribe()
-    }else if(searchResult.includes(this.spstr)){
-      let name = searchResult.split(" ", 2);
-      // let fname = name.findIndex(0)
-      // this.sServ.searchUserByName(name, name.indexOf(1)).subscribe()
-    }else{
-
-      // this.sServ.searchUserByName(searchResult).subscribe()
-    }
+  getUserId(user: User){
+    // user = this.uServ.getUserById(user.userId);
+    return user.userId;
   }
+
+  // findAPerson(searchResult: string){
+  //   if(searchResult.includes(this.emstr)){
+  //     this.sServ.searchUserByEmail(searchResult).subscribe(
+  //       response => {
+  //         this.userList = response;
+  //       }
+  //     )
+  //   }else if(searchResult.includes(this.spstr)){
+  //     let name = searchResult.split(" ", 2);
+  //     let fname = name[0];
+  //     let lname = name[1];
+  //     this.sServ.searchUserByName(fname, lname).subscribe(
+  //       response => {
+  //         this.userList = response;
+  //       }
+  //     )
+  //   }else {
+      
+  //   }
+  // }
 
 }
